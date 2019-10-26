bad385b097ad4f2103e686e3c045d9f30471c561
sunkink29 <calebparks29@gmail.com>
2019-10-26 06:21:22 -0500

    Update settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -6,18 +6,20 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  var _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Settings"),
-        centerTitle: true,
-      ),
-      body: ListView(
-        children: <Widget>[
-          
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Settings"),
+          centerTitle: true,
+        ),
+        body: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField()
+            ],
+          ),
+        ));
   }
 }
